cf348ac5b8adbfd9deef184a3ac544c28482c860
Egor <sapronov.egor@gmail.com>
2015-07-04 13:34:36 +0600

    access models

--- libs/database/database.js
+++ libs/database/database.js
@@ -3,19 +3,22 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/dostavka');
 var db = mongoose.connection;
-var RequestSchema = require('./models/request');
-var RequestModel = mongoose.model('Request', RequestSchema);
+var RequestModel = mongoose.model('Request', require('./models/request'));
+var UserModel = mongoose.model('User', require('./models/user'));
+var AccessTokenModel = mongoose.model('AccessToken', require('./models/token'));
 
 db.on('error', function (err) {
-    console.log(err.message);
+  console.log(err.message);
 });
 
 db.once('open', function () {
-    console.log('connected to db');
+  console.log('connected to db');
 });
 
 module.exports = {
-    db: db,
-    mongoose: mongoose,
-    RequestModel: RequestModel
+  db: db,
+  mongoose: mongoose,
+  RequestModel: RequestModel,
+  UserModel: UserModel,
+  AccessTokenModel: AccessTokenModel
 };
