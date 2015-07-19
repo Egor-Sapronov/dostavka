d47ee05796822b46945bfcfde98fb95d2755b7e8
Egor <sapronov.egor@gmail.com>
2015-07-19 19:13:17 +0600

    ladning

--- web/src/landing.js
+++ web/src/landing.js
@@ -1,3 +1,8 @@
 import '../../bower_components/materialize/dist/css/materialize.css';
 import './css/landing.styl'
 import '../../bower_components/materialize/dist/js/materialize.js';
+
+$('.button-collapse').sideNav({
+  edge:'right',
+  closeOnClick:true
+});
