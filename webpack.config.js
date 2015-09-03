83d71447f21d5833d5819510dd033dac0c5e967d
Dan Abramov <dan.abramov@gmail.com>
2015-09-03 04:35:50 +0300

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,26 @@
-'use strict';
-
 var path = require('path');
-var BowerWebpackPlugin = require('bower-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
+
 module.exports = {
-  context: path.join(__dirname, 'web/src'),
-  entry: {
-    index: './index',
-    requests: './requests',
-    edit: './edit',
-    landing: './landing'
-  },
+  devtool: 'eval',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
   output: {
-    path: path.join(__dirname, 'web/dist'),
-    filename: '[name].js'
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-      },
-      {
-        test: /\.png$/,
-        loader: "url-loader?limit=100000"
-      },
-      {
-        test: /\.jpg$/,
-        loader: "file-loader"
-      },
-      {
-        test: /\.woff2$/,
-        loader: "url?limit=10000&minetype=application/font-woff2"
-      },
-      {
-        test: /\.woff$/,
-        loader: "url?limit=10000&minetype=application/font-woff"
-      },
-      {
-        test: /\.ttf$/,
-        loader: "url?limit=10000&minetype=application/octet-stream"
-      },
-      {
-        test: /\.eot$/,
-        loader: "file"
-      },
-      {
-        test: /\.svg$/,
-        loader: "url?limit=10000&minetype=image/svg+xml"
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.styl$/,
-        loader: 'style-loader!css-loader!stylus-loader'
-      }
-    ]
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery"
-    }),
-    new webpack.optimize.UglifyJsPlugin(),
-    new ExtractTextPlugin("[name].css"),
-    new BowerWebpackPlugin({
-      modulesDirectories: ['bower_components'],
-      manifestFiles: ['bower.json', '.bower.json'],
-      includes: /.*/,
-      excludes: /.*\.less$/
-    })
-  ]
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
 };
