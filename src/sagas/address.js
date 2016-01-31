c2c2040fbe38d69336513b2fc86a2ff78e663a67
Егор <egor@MacBook-Pro-mac.local>
2016-01-31 19:09:40 +0600

    test address saga

--- src/sagas/address.js
+++ src/sagas/address.js
@@ -2,7 +2,7 @@ import { take, put, call } from 'redux-saga';
 import { REQUEST_SEARCH, receiveSearch } from '../actions/address';
 import { searchGeo } from '../io/api';
 
-function* searchAddress() {
+export function* searchAddress() {
     while (true) { // eslint-disable-line no-constant-condition
         const { search, uuid } = yield take(REQUEST_SEARCH);
 
