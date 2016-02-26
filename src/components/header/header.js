95e22327eb33de71ccb7eec164927b806759cc56
6thSence <d.pushkarskaya.pr@gmail.com>
2016-02-27 00:01:11 +0600

    header finish

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,24 @@ import Cart from '../cart/cart';
 export default () => {
     return (
         <div className={ styles.wrap }>
-            <div className={ styles.contacts }></div>
+            <div className={ styles.contacts }>
+                <div className={ styles.icon_call }></div>
+                <div className={ styles.text_wrap }>
+                    <p className={ styles.text }>звоните нам бесплатно:</p>
+                    <p className={ styles.tell }>8-913-777-66-55</p>
+                </div>
+            </div>
+            <div className={ styles.auth }>
+                <a className={ styles.link }>
+                    <div className={ styles.lk_icon }></div>
+                    <span className={ styles.cont_text }>вход</span>
+                </a>
+                <a className={ styles.link }>
+                    <div className={ styles.auth_icon }></div>
+                    <span className={ styles.cont_text }>авторизация</span>
+                </a>
+            </div>
+
             <Cart />
         </div>
     );
