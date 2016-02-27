f9d9fe011888cfdc6dd7b5c0247fc4c80e69fce5
6thSence <d.pushkarskaya.pr@gmail.com>
2016-02-27 15:18:48 +0600

    fix-width

--- src/components/cardsOfFood/cardsOfFood.js
+++ src/components/cardsOfFood/cardsOfFood.js
@@ -8,82 +8,100 @@ export default () => {
                 <li className={ styles.card }>
                     <div alt="тут будет альт" className={ styles.photo }></div>
                     <div className={ styles.info }>
-                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
-                        <div className={ styles.minus }><span className={ styles.sumbl}>-</span></div>
-                        <div className={ styles.plus }><span className={ styles.sumbl}>+</span></div>
                         <p className={ styles.name }>Cola-Mui</p>
+                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
+                    </div>
+                    <div className={ styles.control }>
+                        <div className={ styles.minus }>-</div>
+                        <div className={ styles.plus }>+</div>
                     </div>
                 </li>
                 <li className={ styles.card }>
                     <div alt="тут будет альт" className={ styles.photo }></div>
                     <div className={ styles.info }>
-                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
-                        <div className={ styles.minus }><span className={ styles.sumbl}>-</span></div>
-                        <div className={ styles.plus }><span className={ styles.sumbl}>+</span></div>
                         <p className={ styles.name }>Cola-Mui</p>
+                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
+                    </div>
+                    <div className={ styles.control }>
+                        <div className={ styles.minus }>-</div>
+                        <div className={ styles.plus }>+</div>
                     </div>
                 </li>
                 <li className={ styles.card }>
                     <div alt="тут будет альт" className={ styles.photo }></div>
                     <div className={ styles.info }>
-                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
-                        <div className={ styles.minus }><span className={ styles.sumbl}>-</span></div>
-                        <div className={ styles.plus }><span className={ styles.sumbl}>+</span></div>
                         <p className={ styles.name }>Cola-Mui</p>
+                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
+                    </div>
+                    <div className={ styles.control }>
+                        <div className={ styles.minus }>-</div>
+                        <div className={ styles.plus }>+</div>
                     </div>
                 </li>
                 <li className={ styles.card }>
                     <div alt="тут будет альт" className={ styles.photo }></div>
                     <div className={ styles.info }>
-                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
-                        <div className={ styles.minus }><span className={ styles.sumbl}>-</span></div>
-                        <div className={ styles.plus }><span className={ styles.sumbl}>+</span></div>
                         <p className={ styles.name }>Cola-Mui</p>
+                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
+                    </div>
+                    <div className={ styles.control }>
+                        <div className={ styles.minus }>-</div>
+                        <div className={ styles.plus }>+</div>
                     </div>
                 </li>
                 <li className={ styles.card }>
                     <div alt="тут будет альт" className={ styles.photo }></div>
                     <div className={ styles.info }>
-                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
-                        <div className={ styles.minus }><span className={ styles.sumbl}>-</span></div>
-                        <div className={ styles.plus }><span className={ styles.sumbl}>+</span></div>
                         <p className={ styles.name }>Cola-Mui</p>
+                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
+                    </div>
+                    <div className={ styles.control }>
+                        <div className={ styles.minus }>-</div>
+                        <div className={ styles.plus }>+</div>
                     </div>
                 </li>
                 <li className={ styles.card }>
                     <div alt="тут будет альт" className={ styles.photo }></div>
                     <div className={ styles.info }>
-                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
-                        <div className={ styles.minus }><span className={ styles.sumbl}>-</span></div>
-                        <div className={ styles.plus }><span className={ styles.sumbl}>+</span></div>
                         <p className={ styles.name }>Cola-Mui</p>
+                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
+                    </div>
+                    <div className={ styles.control }>
+                        <div className={ styles.minus }>-</div>
+                        <div className={ styles.plus }>+</div>
                     </div>
                 </li>
                 <li className={ styles.card }>
                     <div alt="тут будет альт" className={ styles.photo }></div>
                     <div className={ styles.info }>
-                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
-                        <div className={ styles.minus }><span className={ styles.sumbl}>-</span></div>
-                        <div className={ styles.plus }><span className={ styles.sumbl}>+</span></div>
                         <p className={ styles.name }>Cola-Mui</p>
+                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
+                    </div>
+                    <div className={ styles.control }>
+                        <div className={ styles.minus }>-</div>
+                        <div className={ styles.plus }>+</div>
                     </div>
                 </li>
                 <li className={ styles.card }>
                     <div alt="тут будет альт" className={ styles.photo }></div>
                     <div className={ styles.info }>
-                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
-                        <div className={ styles.minus }><span className={ styles.sumbl}>-</span></div>
-                        <div className={ styles.plus }><span className={ styles.sumbl}>+</span></div>
                         <p className={ styles.name }>Cola-Mui</p>
+                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
+                    </div>
+                    <div className={ styles.control }>
+                        <div className={ styles.minus }>-</div>
+                        <div className={ styles.plus }>+</div>
                     </div>
                 </li>
                 <li className={ styles.card }>
                     <div alt="тут будет альт" className={ styles.photo }></div>
                     <div className={ styles.info }>
-                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
-                        <div className={ styles.minus }><span className={ styles.sumbl}>-</span></div>
-                        <div className={ styles.plus }><span className={ styles.sumbl}>+</span></div>
                         <p className={ styles.name }>Cola-Mui</p>
+                        <span className={ styles.sum }>234<span className={ styles.rubl }>i</span></span>
+                    </div>
+                    <div className={ styles.control }>
+                        <div className={ styles.minus }>-</div>
+                        <div className={ styles.plus }>+</div>
                     </div>
                 </li>
             </ul>
