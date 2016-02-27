911b0d56ec004620d47a54761e96298eebc7d6f9
6thSence <d.pushkarskaya.pr@gmail.com>
2016-02-27 23:21:29 +0600

    order-page

--- src/components/basket/basket.js
+++ src/components/basket/basket.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './basket.css';
 import CardsOfFood from '../cardsOfFood/cardsOfFood';
-
+import OrderForm from '../orderForm/orderForm';
 
 export default () => {
     return (
         <div className={ styles.wrap }>
             <CardsOfFood itsBasket={ true } />
+            <OrderForm />
         </div>
     );
 };
--- src/components/sumTable/sumTable.js
+++ src/components/sumTable/sumTable.js
@@ -7,7 +7,7 @@ export default () => {
             <div className={ styles.content }>
                 <span className={ styles.text }>сумма вашего заказа: </span>
                 <span className={ styles.sum }> 2999 <span className={ styles.rubl }>i</span></span>
-                <span className={ styles.goToOrder } href="#order">оформить заказ</span>
+                <span className={ styles.goToOrder } href="#createOrder">оформить заказ</span>
             </div>
 
         </div>
